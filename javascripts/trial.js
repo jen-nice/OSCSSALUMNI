88985f975752ad270b5e92684080091c7856d8ac
jencol <jennice.colaco@gmail.com>
2015-05-06 01:07:23 -0400

    Update trial.js

--- javascripts/trial.js
+++ javascripts/trial.js
@@ -1,7 +1,17 @@
-$('.trialme').click(function() {
+var main= function(){
+    $('.trialme').click(function() {
     $('.trialme').animate({
       left:"285px"
     }, 200);
   });
+  
+   $('.thumbnails').hover(function() {
+    $('.thumbnail').animate({
+      left:"285px"
+    }, 200);
+  });
+  
+  
+  };
   
   $(document).ready(main);
