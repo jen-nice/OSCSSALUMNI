3814d5b60694c3cc444523e95102f596053d3598
jencol <jennice.colaco@gmail.com>
2015-05-11 14:08:09 -0400

    changed to active slide click

--- javascripts/pictures.js
+++ javascripts/pictures.js
@@ -1,24 +1,5 @@
-
-/*var main = function(){
- $('.glyphicon glyphicon-chevron-right').click(function(){
- var currentSlide =$('.active-slide');
- var nextSlide = currentSlide.next();
- 
- currentSlide.fadeOut(600).removeClass('active-slide');
- nextSlide.fadeIn(600).addClass('active-slide');
- });
- 
- $('.glyphicon glyphicon-chevron-left').click(function(){
-  var currentSlide =$('.active-slide');
- var nextSlide = currentSlide.prev();
- 
- currentSlide.fadeOut(600).removeClass('active-slide');
- nextSlide.fadeIn(600).addClass('active-slide');	
- });
-}*/
-
 var main = function(){
- $('.arrow-next').click(function() {
+ $('.active-slide').click(function() {
     var currentSlide = $('.active-slide');
     var nextSlide = currentSlide.next();
 
