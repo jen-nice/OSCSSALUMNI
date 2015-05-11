43da8110c534fda4a91efa8f2217f605d8c118b7
jencol <jennice.colaco@gmail.com>
2015-05-10 23:14:11 -0400

    Dropdown menu toggle

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,7 +1,14 @@
 var main = function(){
+ //Dropdown menu
+ $(.dropdown-toggle).click(function(){
+   $(.dropdown-menu).toggle();
+ });
+ 
+ //For the about toggle
  $('#pagetopic').click(function(){
   $('#pagetopic').fadeOut(1000);
  });
+ 
 };
 
 $(document).ready(main);
